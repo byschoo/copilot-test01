162a4bf1ab31e2b5fd67b0814db7365eea34e463
127136013+byschoo@users.noreply.github.com <byschoo@outlook.com>
2023-04-19 18:40:22 +0200

    Primera versión de la aplicación de prueba de operaciones aritméticas.

---
+++ Source/copilo-test01-operaciones.cpp
@@ -0,0 +1,21 @@
+//Write a program that reads two numbers and writes their addition, subtraction, multiplication, and division. Rounded to two decimal places of precision
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    double a, b;
+    cout << "Enter two numbers: " << endl;
+    cin >> a;
+    cin >> b;
+    cout << endl;
+    cout << "The sum is " << fixed << setprecision(2) << a + b << endl;
+    cout << "The difference is " << fixed << setprecision(2) << a - b << endl;
+    cout << "The product is " << fixed << setprecision(2) << a * b << endl;
+    cout << "The quotient is " << fixed << setprecision(2) << a / b << endl;
+    return 0;
+}
+
+
+
+
